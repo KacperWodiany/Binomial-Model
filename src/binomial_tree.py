2a391f16cf08d87ba1ff0f4a705289b4ff0b61ad
Kacper Wodiany <kwodiany@gmail.com>
2021-01-05 20:48:46 +0100

    Fix tree generation and add dividend assumption
    Initial price is no longer unnecessarily incremented by drift term.
    Paths' increments are computed now taking dividend yield into account.

--- src/binomial_tree.py
+++ src/binomial_tree.py
@@ -5,13 +5,16 @@ import tree_paths
 # Works fine for path independent options (European, Bermuda, American)
 class BinomialTree:
 
-    def __init__(self, periods, mean, std, rate, s0, num0=1):
+    def __init__(self, periods, mean, std, rate, s0, num0=1, div_yield=0, div_periods=0):
         self.periods = periods
         self.mean = mean
         self.std = std
         self.rate = rate
         self.s0 = s0
         self.num0 = num0
+        # If dividend is not introduced setting both values to 0 don't hurt results nor performance
+        self.div_yield = div_yield
+        self.div_periods = div_periods
         self.q = None
         self.tree = None
 
@@ -19,18 +22,23 @@ class BinomialTree:
         self._find_emm()
         tree_gen_mx = self._generate_increments()
         # calculate tree of discounted prices
-        tree_gen_mx = np.cumsum((self.mean - self.rate) + self.std * tree_gen_mx, axis=1)
-        self.tree = self.s0 / self.num0 * np.exp(tree_gen_mx)
+        # drift term is excluded from cumulative summing to prevent adding it to initial price
+        tree_gen_mx = np.cumsum(self.std * tree_gen_mx, axis=1)
+        tree_gen_mx = self.s0 / self.num0 * np.exp(tree_gen_mx)
+        # add drift term
+        self.tree = tree_gen_mx * np.exp((self.mean - self.rate) * np.arange(0, self.periods + 1))
 
     def _find_emm(self):
         # TODO: Take dividend yield into account
         self.q = (np.exp(self.rate - self.mean) - np.exp(-self.std)) / (np.exp(self.std) - np.exp(-self.std))
 
     def _generate_increments(self):
-        tree_gen_mx = np.triu(np.ones((self.periods + 1, self.periods + 1)), k=0)
+        increments = np.triu(np.ones((self.periods + 1, self.periods + 1)), k=0)
         # fill diagonal with sequence 0, -1, -2, ..., -T
-        np.fill_diagonal(tree_gen_mx, -np.arange(0, self.periods + 1))
-        return tree_gen_mx
+        np.fill_diagonal(increments, -np.arange(0, self.periods + 1))
+        # apply dividend assumption
+        increments[:, self.div_periods] -= np.log(1 + self.div_yield) / self.std
+        return increments
 
     def calculate_U(self, payoff):
         h = self._calculate_H(payoff)
