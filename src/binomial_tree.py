90ee2b5642b92aeb8ac200de82715a47a48120c7
Kacper Wodiany <kwodiany@gmail.com>
2021-01-09 20:48:31 +0100

    Fix envelope and expectation calculations issues
    While creating an envelope maximum should be taken after each iteration
    (not after all iterations, over whole matrices).
    
    While calculating expectation of future values of envelope, the array
    should be sorted with respect to original order of stock prices, not
    the order of envelope itself.
    
    Parameters in test were changed.

--- src/binomial_tree.py
+++ src/binomial_tree.py
@@ -5,8 +5,8 @@ import tree_paths
 # Works fine for path independent options (European, Bermuda, American)
 class BinomialTree:
 
-    def __init__(self, periods, mean, std, rate, s0, num0=1, div_yield=0, div_periods=0):
-        self.periods = periods
+    def __init__(self, nb_periods, mean, std, rate, s0, num0=1, div_yield=0, div_periods=0):
+        self.nb_periods = nb_periods
         self.mean = mean
         self.std = std
         self.rate = rate
@@ -27,15 +27,14 @@ class BinomialTree:
         tree_gen_mx = self.s0 / self.num0 * np.exp(tree_gen_mx)
         # apply dividend assumption
         div_yield_seq = self._create_dividend_yield_seq()
-        # tree_gen_mx[:, self.div_periods] = tree_gen_mx[:, self.div_periods] / div_yield_seq
         tree_gen_mx /= div_yield_seq
         # add drift term and assign attribute
-        self.tree = tree_gen_mx * np.exp((self.mean - self.rate) * np.arange(0, self.periods + 1))
+        self.tree = tree_gen_mx * np.exp((self.mean - self.rate) * np.arange(0, self.nb_periods + 1))
 
     def _find_emm(self):
         # Create vector of dividend yields
         # Set 0 if no dividend assumed for the period
-        dividends = np.zeros(self.periods + 1)
+        dividends = np.zeros(self.nb_periods + 1)
         # Set relevant value from div_yield if dividend is assumed
         dividends[self.div_periods] = self.div_yield
         # Calculate probability based taking dividends into account
@@ -46,50 +45,63 @@ class BinomialTree:
                 / (2 * np.sinh(self.std)))
 
     def _generate_increments(self):
-        increments = np.triu(np.ones((self.periods + 1, self.periods + 1)), k=0)
+        increments = np.triu(np.ones((self.nb_periods + 1, self.nb_periods + 1)), k=0)
         # fill diagonal with sequence 0, -1, -2, ..., -T
-        np.fill_diagonal(increments, -np.arange(0, self.periods + 1))
+        np.fill_diagonal(increments, -np.arange(0, self.nb_periods + 1))
         return increments
 
     def _create_dividend_yield_seq(self):
         # If no dividend assumed sequence consists of ones
-        dividend_seq = np.ones(self.periods + 1)
+        dividend_seq = np.ones(self.nb_periods + 1)
         try:
             # When arrays of dividend periods and yields are passed, the array above must have products of consecutive
             # yields on respective places
             for t, d in zip(self.div_periods, self.div_yield):
                 dividend_seq[t:] *= (1 + d)
         except TypeError:
-            # If dividend periods are ints, we cannot iterate over them
+            # If dividend periods and yields are ints, we cannot iterate over them
             dividend_seq[self.div_periods:] *= (1 + self.div_yield)
-        else:
-            # If tree is sliced with array the sequence needs to be 2-D
-            dividend_seq = np.array([dividend_seq, ] * (self.periods + 1))
+
+        dividend_seq = np.array([dividend_seq, ] * (self.nb_periods + 1))
         return dividend_seq
 
-    def calculate_U(self, payoff):
-        h = self._calculate_H(payoff)
-        u = np.copy(h)
-        for t in range(1, self.periods + 1):
+    def calculate_envelope_tree(self, payoff_tree):
+        envelope_tree = np.copy(payoff_tree)
+        for t in range(1, self.nb_periods + 1):
             # calculate expectation of future value of Snell's Envelope - E(U_{t+1}|F_t)
-            prev_u = self.q[-t + 1] * u[:-1, -t] + (1 - self.q[-t + 1]) * u[1:, -t]
-            # fill redundant entries with 0
-            u[:, -(t + 1)] = np.hstack((prev_u, 0))
-        return np.maximum(u, h)
+            previous = self.q[-t] * envelope_tree[:-1, -t] + (1 - self.q[-t]) * envelope_tree[1:, -t]
+            # Take maximum of Ut and Ht (envelope and payoff at time t)
+            # the array is 1 element shorter than expected
+            # lower entries are filled with some numbers, but aren't used. No need to take care of them
+            envelope_tree[:-1, -(t+1)] = np.maximum(previous, payoff_tree[:-1, -(t+1)])
+        return envelope_tree
 
-    def _calculate_H(self, payoff):
+    def calculate_payoff_tree(self, payoff_func, **option_params):
         # payoff function must handle 2-D array
-        return payoff(self.tree)
+        return payoff_func(self.tree, **option_params)
 
-    def calculate_expectation(self, omega, periods=None, tree=None):
-        if tree is None:
+    def calculate_expectation(self, omega, tree=None):
+        try:
+            # Try to get path and its neighbors values for given periods
+            path = tree_paths.extract_with_neighbors(tree, omega)
+        except AttributeError:
+            # Assign classes self.tree if tree wasn't passed
             tree = self.tree
-        if periods is None:
-            periods = np.arange(self.periods + 1)
-        # Get path and its neighbors values for given periods
-        path = tree_paths.extract_with_neighbors(tree, omega)[:, periods]
-        # Put greater values in upper row and smaller values in lower row
-        path = -np.sort(-path[:, 1:], axis=0)
+            # Get path and its neighbors values for given periods
+            path = tree_paths.extract_with_neighbors(tree, omega)
+        else:
+            # Create mask for sorting path
+            mask_path = tree_paths.extract_with_neighbors(self.tree, omega)
+        try:
+            # try to create mask for sorting
+            mask = np.argsort(-mask_path[:, 1:], axis=0)
+        except NameError:
+            # If path for creating mask was not extracted, just sort the path
+            sorted_path = -np.sort(-path[:, 1:], axis=0)
+        else:
+            # if path for creating mask was extracted, sort the path with respect to order of stock prices in the tree
+            sorted_path = np.take_along_axis(path[:, 1:], mask, axis=0)
+
         # Arrange matrix of probabilities
         probabilities = np.array([self.q, 1 - self.q])
-        return np.sum(probabilities * path, axis=0)
+        return np.sum(probabilities * sorted_path, axis=0)
